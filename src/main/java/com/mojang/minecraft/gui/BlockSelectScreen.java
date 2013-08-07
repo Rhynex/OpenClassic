7fd0dfafb39c43c47ee885ca8efbad69dc143742
Rhynex <axel943@gmail.com>
2013-08-07 21:03:56 +0200

    Added 3 alternative block choosers: default, modified and fancy (fancy works best with limited framerate on).

--- src/main/java/com/mojang/minecraft/gui/BlockSelectScreen.java
+++ src/main/java/com/mojang/minecraft/gui/BlockSelectScreen.java
@@ -6,11 +6,16 @@ import ch.spacebase.openclassic.api.OpenClassic;
 import ch.spacebase.openclassic.api.block.BlockType;
 import ch.spacebase.openclassic.api.block.Blocks;
 import ch.spacebase.openclassic.api.gui.GuiScreen;
+import ch.spacebase.openclassic.api.gui.widget.Button;
+import ch.spacebase.openclassic.api.math.MathHelper;
 import ch.spacebase.openclassic.api.render.RenderHelper;
 import ch.spacebase.openclassic.client.util.GeneralUtils;
 
 public final class BlockSelectScreen extends GuiScreen {
 
+	private float rotateAngle, pitchAngle, localSine, localSineModifier = 0.0F;
+	private boolean fancy;
+	
 	public BlockSelectScreen() {
 		this.setGrabsInput(false);
 	}
@@ -45,18 +50,52 @@ public final class BlockSelectScreen extends GuiScreen {
 		}
 
 		RenderHelper.getHelper().renderText(OpenClassic.getGame().getTranslator().translate("gui.blocks.select"), this.getWidth() / 2, 40);
-
+		
 		int count = 0;
 		for(BlockType b : Blocks.getBlocks()) {
 			if(b != null && b.isSelectable()) {
+				switch (GeneralUtils.getMinecraft().settings.getIntSetting("options.blockChooser").getValue()) {
+					case 0: {
+						this.rotateAngle = -45.0F; 
+						this.pitchAngle = -30.0F; 
+						this.fancy = false;
+						break;
+					}
+					case 1: {
+						this.rotateAngle = -30.0F; 
+						this.pitchAngle = -20.0F;
+						fancy = false;
+						break;
+					}
+					case 2: {
+						this.pitchAngle = -25.0F;
+						this.fancy = true;
+						break;
+					}
+				}
+				
 				GL11.glPushMatrix();
 				GL11.glTranslatef(this.getWidth() / 2 + count % 9 * 24 + -108, this.getHeight() / 2 + count / 9 * 24 + -60, 0);
 				GL11.glScalef(10.0F, 10.0F, 10.0F);
 				GL11.glTranslatef(1.0F, 0.5F, 8.0F);
-				GL11.glRotatef(-30.0F, 1.0F, 0.0F, 0.0F);
-				GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-				if(block == count) {
-					GL11.glScalef(1.6F, 1.6F, 1.6F);
+				GL11.glRotatef(pitchAngle, 1.0F, 0.0F, 0.0F);
+				
+				if (this.fancy) {
+					if (rotateAngle >= 360.0F) rotateAngle = 0.0F;
+					if (localSineModifier >= 360.0F) localSineModifier = 0.0F;
+					rotateAngle += 0.006F;
+					localSineModifier += 0.06F;
+					
+					localSine = MathHelper.sin(localSineModifier * MathHelper.DEG_TO_RAD);
+					if(block == count) {
+						GL11.glTranslatef(0.0F, 0.15F * localSine, 0.0F);
+					}
+				}
+				
+				GL11.glRotatef(rotateAngle, 0.0F, 1.0F, 0.0F);
+
+				if (block == count) {
+					GL11.glScalef(1.55F, 1.55F, 1.55F);
 				}
 
 				GL11.glTranslatef(-1.5F, 0.5F, 0.5F);
